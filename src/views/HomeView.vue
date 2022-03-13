c4ff0d28f3a049337514733892c04bab047395c0
nragan34 <deadpool@LAPTOP-IH0Q8U3J.localdomain>
2022-03-13 17:44:58 -0500

    initial commit (2)

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+</script>
+
+<template>
+  <main>
+      <h1>hi</h1>
+    The Count: {{count}}
+    {{ 20 + 20 }}
+  </main>
+</template>
---
+++ src/views/samples/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import TheWelcome from "@/components/TheWelcome.vue";
+import { ref } from 'vue'
+const count = ref(0)
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    The Count: {{count}}
+  </main>
+</template>
